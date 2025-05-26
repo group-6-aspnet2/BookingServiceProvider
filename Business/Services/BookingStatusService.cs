83a6cb2cd6b743800c6e99156b351c1a60bc4f93
PetraElgemyr <petra.elgemyr@gmail.com>
2025-05-26 20:11:41 +0200

    update bookingservice getAll and bookingstatus getAll with inmemorycache. different expiration times

--- Business/Services/BookingStatusService.cs
+++ Business/Services/BookingStatusService.cs
@@ -1,6 +1,7 @@
 ﻿using Data.Interfaces;
 using Domain.Models;
 using Domain.Responses;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 
 namespace Business.Services;
@@ -11,26 +12,46 @@ public interface IBookingStatusService
     Task<BookingStatusResult<StatusModel>> GetByIdAsync(int id);
 }
 
-public class BookingStatusService(IBookingStatusRepository bookingStatusRepository) : IBookingStatusService
+public class BookingStatusService(IBookingStatusRepository bookingStatusRepository, IMemoryCache cache) : IBookingStatusService
 {
 
     private readonly IBookingStatusRepository _bookingStatusRepository = bookingStatusRepository;
+    private readonly IMemoryCache _cache = cache;
+    private readonly TimeSpan _cacheExpiration = TimeSpan.FromMinutes(30);
+
 
     public async Task<BookingStatusResult<IEnumerable<StatusModel>>> GetAllAsync()
     {
         try
         {
-            var result = await _bookingStatusRepository.GetAllAsync();
-            if (!result.Succeeded)
-                return new BookingStatusResult<IEnumerable<StatusModel>> { Error = "Could not fetch booking statuses", StatusCode = 404, Succeeded = false };
+            const string cacheKey = "StatusTypes";
 
+            if(!_cache.TryGetValue(cacheKey, out IEnumerable<StatusModel>? statuses))
+            {
+                var result = await _bookingStatusRepository.GetAllAsync();
+                if (!result.Succeeded)
+                    return new BookingStatusResult<IEnumerable<StatusModel>> { Error = "Could not fetch booking statuses", StatusCode = 404, Succeeded = false };
+
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(_cacheExpiration);
+                _cache.Set(cacheKey, result.Result, cacheEntryOptions);
+
+                return new BookingStatusResult<IEnumerable<StatusModel>>
+                {
+                    Result = result.Result,
+                    StatusCode = 200,
+                    Succeeded = true
+                };
+            }
 
             return new BookingStatusResult<IEnumerable<StatusModel>>
             {
-                Result = result.Result,
+                Result = statuses,
                 StatusCode = 200,
                 Succeeded = true
             };
+
+
         }
         catch (Exception ex)
         {
